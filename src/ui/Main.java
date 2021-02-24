73bf9b769e4808ec5f297524e0f0e54f72998ea5
Estebanm1812 <69942961+Estebanm1812@users.noreply.github.com>
2021-02-24 11:03:44 -0500

    first commit

---
+++ src/ui/Main.java
@@ -0,0 +1,28 @@
+package ui;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String line = br.readLine();
+		
+		int quantity = Integer.parseInt(br.readLine());
+		
+		while((line != null)) {
+		
+			for(int i =0; i < quantity;i++) {
+		
+			String textValue = br.readLine();
+			
+			
+			
+			}	
+		}	
+	}
+
+}
