9cdf30bf6cfe1014eb31c4e3cf7cededfc7546ab
Estebanm1812 <69942961+Estebanm1812@users.noreply.github.com>
2021-02-24 17:33:44 -0500

    Amount of changes added

--- src/ui/Main.java
+++ src/ui/Main.java
@@ -19,11 +19,31 @@ public class Main {
 		
 		
 			String lineValue = br.readLine();
-			String [] textValue = lineValue.split(" ");	
+			String [] textValue = lineValue.split(" ");
+			double [] value = new double [textValue.length];
 			for(int i=0; i < textValue.length;i++) { 
 				
+				value[i] = Double.parseDouble(textValue[i]);
 				
-			amount+=1;
+			
+			}
+			int totalchanges = 0;
+			for(int i =1; i < value.length;i--) {
+					
+					
+					int changes = 0;
+					
+					for(int j = 0; j < value.length - i ; j++ ) {
+				
+						if(value[j]>value[i+1]) {
+						
+							double temp = value[j];
+							value[j] = value[j+1];
+							value[j+1] = temp;
+							changes+=1;
+						}
+					totalchanges+= changes;	
+					}
 			}
 		}	
 	}
